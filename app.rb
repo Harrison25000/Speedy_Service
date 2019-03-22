19323d3cd6f61de3ff990daeaa91423e2de22ee5
Oliver <ofcharrison@gmail.com>
2019-03-22 10:54:55 +0000

    Login functioning, have to signup to use

--- app.rb
+++ app.rb
@@ -4,11 +4,25 @@ require './lib/users.rb'
 class Services < Sinatra::Base
   enable :sessions
 
-  get '/signin' do
-    erb :index
+  #SIGNIN ----------------------------------------------------------
+
+  before  do
+    path = request.path_info
+      if (path != "/signin"  && path != '/signup') && !session[:userid]
+      redirect("/signin")
+    end
+  end
+
+  get '/' do
+    redirect '/signin'
+  end
+
+  get '/error' do
+    erb :error
   end
 
   get '/welcome' do
+    @userid = session[:userid]
     erb :welcome
   end
 
@@ -25,6 +39,23 @@ class Services < Sinatra::Base
     session[:userid] = @user.id
     redirect "/signin"
   end
+
+  get '/signin' do
+    erb :index
+  end
+
+  post '/signin' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:userid] = user.id
+      redirect '/welcome'
+    else
+      erb :error
+    end
+  end
+
+  #SIGNIN-------------------------------------------------------------
 
   run! if app_file == $0
 
